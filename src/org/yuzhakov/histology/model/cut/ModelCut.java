2ebf0e5b5ba9bc7eca034e813c62354fdbdae1f0
mr moonshine <mr.moonshine@mail.ru>
2014-06-17 14:38:12 +0400

    Gekko!

---
+++ src/org/yuzhakov/histology/model/cut/ModelCut.java
@@ -0,0 +1,46 @@
+package org.yuzhakov.histology.model.cut;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.yuzhakov.histology.Tetgen;
+import org.yuzhakov.histology.model.Cell;
+
+public class ModelCut {
+	private List<Cell> cells;
+	private CutPlane cutPlane;
+	private List<CellCut> cuts;
+	
+	public ModelCut(List<Cell> cells, CutPlane cutPlane) {
+		super();
+		this.cells = cells;
+		this.cutPlane = cutPlane;
+		this.cuts = createCellCuts(cells, cutPlane);
+	}
+	
+	public List<Cell> getCells() {
+		return cells;
+	}
+
+	public CutPlane getCutPlane() {
+		return cutPlane;
+	}
+
+	public List<CellCut> getCuts() {
+		return cuts;
+	}
+
+	private List<CellCut> createCellCuts(List<Cell> cells, CutPlane cutPlane){
+		List<CellCut> cellCuts = new ArrayList<>();
+		for (Cell cell : cells){
+			Tetgen tetgen = new Tetgen(cell);
+			tetgen.tetrahedralize(null);
+			
+			CellCut cellCut = new CellCut(cutPlane, cell, tetgen.getTetrahedronList());
+			cellCuts.add(cellCut);
+		}
+		
+		return cellCuts;		
+	}
+	
+}
